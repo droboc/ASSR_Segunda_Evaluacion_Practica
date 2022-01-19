28e7938b8ae47474203632a13b741458bc175cd1
droboc <drobles@espol.edu.ec>
2022-01-18 22:27:26 -0500

    Deploy on Herokuu

--- models/routes.model.js
+++ models/routes.model.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose')
 
 
 var routerSchema = new mongoose.Schema({
+    FuelTypeCode:{ type: String, required: ' This field is required', },
+    StationName:{ type: String, required: ' This field is required', },
+    StreetAddress:{ type: String, required: ' This field is required', },
+    IntersectionDirections:{ type: String, required: ' This field is required', },
     City:{ type: String, required: ' This field is required', },
     State:{ type: String, required: ' This field is required', },
     ZIP:{ type: String, required: ' This field is required', },
+    StatusCode:{ type: String, required: ' This field is required', },
+    GroupsWithAccessCode:{ type: String, required: ' This field is required', },
+    AccessDaysTime:{ type: String, required: ' This field is required', },
 });
 
 
